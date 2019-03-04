fc20d7322a4db1b1f795cdd41a84333b2dc890a1
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-03-04 14:04:35 +0000

    init

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CustomerModule } from './customer/customer.module';
+// ./src/app.module.ts
+
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/customer-app', { useNewUrlParser: true }),
+    CustomerModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {}
